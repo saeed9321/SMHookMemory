90bac4b63677aeffb5ecbb3189ad33e83e9df068
Saeed9321 <38338100+saeed9321@users.noreply.github.com>
2022-01-31 21:36:15 +0400

    Update SMHookMemory.h
    added SVC #0x80 scanner and instrumenting

--- SMHookMemory.h
+++ SMHookMemory.h
@@ -1,36 +1,21 @@
-
-
-
 /* 
   Purpose:
       You can access all registers values in debug function when trampoline
-  Parameters:
-      image name
-      address from Ghidra/IDA ...
-      handle function 
-  Return value:
-      none
 */
 extern struct _Registers ctx;
 typedef struct _Registers {
-  union {
-    uint64_t x[29];
-    struct {
-      uint64_t x0, x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14, x15, x16, x17, x18, x19, x20, x21, x22, x23, x24, x25, x26, x27, x28;
-    } regs;
-  } general;
-
-  uint64_t fp;
-  uint64_t lr;
+  uint64_t x0, x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14,
+           x15, x16, x17, x18, x19, x20, x21, x22, x23, x24, x25, x26, x27,
+           x28, fp, lr;
 } Registers;
 
 
 
-/* 
+/*
   Purpose:
       Get base address for specified image
   Parameters:
-      image name
+      image name or NULL for base image
   Return value:
       image slide
 */
@@ -38,12 +23,12 @@ uint64_t image_slide(char *imageName);
 
 
 
-/* 
+/*
   Purpose:
       Get address+aslr for address in specified image name
   Parameters:
+      image name or NULL for base image
       address from Ghidra/IDA ...
-      image name
   Return value:
       real address
 */
@@ -51,7 +36,7 @@ uint64_t real_address(char *imageName, uint64_t address);
 
 
 
-/* 
+/*
   Purpose:
       Generate hex bytes from arm64 instruction
   Parameters:
@@ -65,11 +50,11 @@ unsigned char *get_hex_from_instruction(const char* inst, size_t *size);
 
 
 
-/* 
+/*
   Purpose:
       Patch arm64 instruction at specified address and image name
   Parameters:
-      image name
+      image name or NULL for base image
       address from Ghidra/IDA ...
       arm64 instruction ex: ret OR mov x0, 0x1
   Return value:
@@ -80,14 +65,39 @@ bool SMHookMemory(char *imageName, uint64_t addr, const char* inst);
 
 
 
-/* 
+/*
+  Purpose:
+      DBI handler function to printf registers value
+  Parameters:
+      none
+  Return value:
+      none
+*/
+void printRegisters(void);
+
+
+
+/*
   Purpose:
       Dynamic Binary Instrumentation
   Parameters:
-      image name
+      image name or NULL for base image
       address from Ghidra/IDA ...
-      handle function 
+      handle function
   Return value:
       none
 */
 void SMInstrument(char *imageName, uint64_t addr, void* handle);
+
+
+
+/*
+  Purpose:
+      Dynamic Binary Instrumentation of all SVC #0x80 in the specified image
+  Parameters:
+      image name or NULL for base image
+      handle function
+  Return value:
+      none
+*/
+void SMInstrumentSVC80(char *imageName, void* handle);
